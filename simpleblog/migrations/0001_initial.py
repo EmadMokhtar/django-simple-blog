2a571b6541dc8fb6af5d904ed4a8d5ee87a721e6
Emad Mokhtar <emad.m.habib@gmail.com>
2016-10-06 16:30:32 +0300

    Add sites latest migration to dependencies

--- simpleblog/migrations/0001_initial.py
+++ simpleblog/migrations/0001_initial.py
@@ -12,6 +12,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
+        ('sites', '__latest__'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
